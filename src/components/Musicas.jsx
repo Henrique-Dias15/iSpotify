aebe199f88fe6990ec4b8ed69d9aa66422015836
Henrique Dias <henriquejnd@proton.me>
2023-11-03 16:04:58 -0300

    Corações verdes funcionando

--- src/components/Musicas.jsx
+++ src/components/Musicas.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 import "./Musicas.css";
+import { useState } from "react";
 
 const Musicas = ({ numero, musica, artista, album, id }) => {
+  const [coração, setCoração] = useState("src/assets/coraçãoDefault.svg");
+
+  const changeCoração = () => {
+    if(coração == "src/assets/coraçãoDefault.svg") {
+      setCoração("src/assets/coraçãoVerde.svg");
+    } else {
+      setCoração("src/assets/coraçãoDefault.svg")
+    }
+  }
+
   return (
     <div className="Musica">
       <div className="MusicasNomes">
@@ -13,8 +24,8 @@ const Musicas = ({ numero, musica, artista, album, id }) => {
       </div>
       <p id="musicaAlbum">{album}</p>
       <div className="MusicasSimbolos">
-        <input type="image" id={id} src="src/assets/coraçãoDefault.svg"/>
-        <img src="src/assets/lixo.svg" id="lixo" />
+        <input type="image" id={id} src={coração} onClick = {() => changeCoração()}/>
+        <img src="src\assets\lixo.svg" id="lixo"/>
       </div>
     </div>
   );
