9ae8c0d8418eca2518d713151d9962a5d73ed78d
Henrique Dias <henriquejnd@proton.me>
2023-11-03 14:51:41 -0300

    Correções

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import Desktop1 from './components/Desktop1.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
       <div>
-        
+        <Desktop1 />
       </div>
   )
 }
