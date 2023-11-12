583398deb0e9074d32ad02426a6343f782399f89
Henrique Dias <henriquejnd@proton.me>
2023-11-12 16:26:02 -0300

    Coração funcionando

--- src/components/Musicas.jsx
+++ src/components/Musicas.jsx
@@ -3,76 +3,83 @@ import "./Musicas.css";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
-
-const Musicas = ({ numero, musica, artista, album, id,}) => {
-  const [coração, setCoração] = useState("src/assets/coraçãoDefault.svg");
-
-
+const Musicas = ({ numero, musica, artista, album, id }) => {
   const [listaMusica, setListaMusica] = useState([
     {
-      numero:"1",
-      musica:"The Zephyr Song",
-      artista:"Red Hot Chili Peppers",
-      album:"By The Way",
+      numero: "1",
+      musica: "The Zephyr Song",
+      artista: "Red Hot Chili Peppers",
+      album: "By The Way",
+      coração: "src/assets/coraçãoDefault.svg",
     },
     {
-      numero:"2",
-      musica:"Talk",
-      artista:"Coldplay",
-      album:"X&Y",
+      numero: "2",
+      musica: "Talk",
+      artista: "Coldplay",
+      album: "X&Y",
+      coração: "src/assets/coraçãoDefault.svg",
     },
     {
       numero: "3",
       musica: "Firmamento",
       artista: "Cidade Negra",
       album: "Cidade Negra Acústico MTV",
+      coração: "src/assets/coraçãoDefault.svg",
     },
   ]);
-  
-  
+
   function removermusica(index) {
-    const novaLista = listaMusica.filter((musica,i) => i !== index)
-    setListaMusica(novaLista)
+    const novaLista = listaMusica.filter((musica, i) => i !== index);
+    setListaMusica(novaLista);
   }
-  
- /*  const handleLixoClick = () => {
-    window.location.replace(desk);
-  }; */
 
-
-  const changeCoração = (index) => {
-    const linhacoração = coração;
-    if(coração == "src/assets/coraçãoDefault.svg") {
-      setCoração("src/assets/coraçãoVerde.svg");
-    } else {
-      setCoração("src/assets/coraçãoDefault.svg")
-    }
+  function changeCoração(index) {
+    setListaMusica(
+      listaMusica.map((musicas, i) => {
+        if (i === index) {
+          if (musicas.coração === "src/assets/coraçãoDefault.svg") {
+            return { ...musicas, coração: "src/assets/coraçãoVerde.svg" };
+          } else {
+            return { ...musicas, coração: "src/assets/coraçãoDefault.svg" };
+          }
+        } else {
+          return { ...musicas };
+        }
+      })
+    );
   }
 
   return (
-        <div>   
-          {listaMusica.map((musica,index) => {
-            return(
-            <div className="Musica"> 
-               <div className="MusicasNomes">
-                  <p id="numero">{musica.numero}</p>
-                  <div> 
-                  <p>{musica.musica}</p>
-                  <p>{musica.artista}</p>
-                  </div>
-                  </div>
-                  <p id="musicaAlbum">{musica.album}</p>
-                  <div className="MusicasSimbolos">
-            <input type="image" id={id} src={coração} onClick = {() => changeCoração(index)}/>
-            <input type="image" id="lixo" src="src\assets\lixo.svg" onClick={ () => removermusica(index)} />
-                   
-                  </div>
-                </div>
-            
-          );
-          })}
-         
-        </div>
+    <div>
+      {listaMusica.map((musica, index) => {
+        return (
+          <div className="Musica">
+            <div className="MusicasNomes">
+              <p id="numero">{index + 1}</p>
+              <div>
+                <p>{musica.musica}</p>
+                <p>{musica.artista}</p>
+              </div>
+            </div>
+            <p id="musicaAlbum">{musica.album}</p>
+            <div className="MusicasSimbolos">
+              <input
+                type="image"
+                id={id}
+                src={musica.coração}
+                onClick={() => changeCoração(index)}
+              />
+              <input
+                type="image"
+                id="lixo"
+                src="src\assets\lixo.svg"
+                onClick={() => removermusica(index)}
+              />
+            </div>
+          </div>
+        );
+      })}
+    </div>
   );
 };
 
